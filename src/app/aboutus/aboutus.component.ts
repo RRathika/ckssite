9996125ffc60f655437443653c44645f42439389
RRathika <shahishaammu03@gmail.com>
2021-11-01 16:35:27 +0530

    mobile responsive for slider

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -19,19 +19,15 @@ export class AboutusComponent implements OnInit {
   data=[
     {
       "image": "https://i.picsum.photos/id/582/536/354.jpg?hmac=yl_995o4eDi7GMlFYMZcpu_DHFYjuA0g83BuhpHyGiI",
-      "thumbImage": "https://i.picsum.photos/id/582/536/354.jpg?hmac=yl_995o4eDi7GMlFYMZcpu_DHFYjuA0g83BuhpHyGiI",
-      "title": "Image One"
+      "thumbImage": "https://i.picsum.photos/id/582/536/354.jpg?hmac=yl_995o4eDi7GMlFYMZcpu_DHFYjuA0g83BuhpHyGiI"
     },
     {
       "image": "https://i.picsum.photos/id/392/536/354.jpg?hmac=pfwZXOXi7mvSPp3AmAeieDhYPDGHv_1e5UCWo-t4McI",
-      "thumbImage": "https://i.picsum.photos/id/392/536/354.jpg?hmac=pfwZXOXi7mvSPp3AmAeieDhYPDGHv_1e5UCWo-t4McI",
-      "alt": "image six"
+      "thumbImage": "https://i.picsum.photos/id/392/536/354.jpg?hmac=pfwZXOXi7mvSPp3AmAeieDhYPDGHv_1e5UCWo-t4McI"
     },
     {
       "image": "https://i.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0",
-      "thumbImage": "https://i.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0",
-      "alt": "alt of image seven",
-      "title": "Image Two"
+      "thumbImage": "https://i.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0"
     },
     {
       "image": "https://i.picsum.photos/id/889/536/354.jpg?hmac=Ed5MAlDO9NIMLSNWrV0nvfDqZfUEzSf9cU6Fc9S_qSM",
@@ -39,58 +35,47 @@ export class AboutusComponent implements OnInit {
     },
     {
       "image": "https://i.picsum.photos/id/922/536/354.jpg?hmac=i5eN3Vt4D9V_ZLK3Wjc7OFW7J9Gj60gZy6FSNl9ycN8",
-      "thumbImage": "https://i.picsum.photos/id/922/536/354.jpg?hmac=i5eN3Vt4D9V_ZLK3Wjc7OFW7J9Gj60gZy6FSNl9ycN8",
-      "title": "Image Four"
+      "thumbImage": "https://i.picsum.photos/id/922/536/354.jpg?hmac=i5eN3Vt4D9V_ZLK3Wjc7OFW7J9Gj60gZy6FSNl9ycN8"
     },
     {
       "image": "https://i.picsum.photos/id/168/889/536.jpg?hmac=3oSZ237Va2MomQuC9nnE7Z2drQYJOQiOES2xb3XBZ_Y",
-      "thumbImage": "https://i.picsum.photos/id/168/889/536.jpg?hmac=3oSZ237Va2MomQuC9nnE7Z2drQYJOQiOES2xb3XBZ_Y",
-      "title": "Image Five"
+      "thumbImage": "https://i.picsum.photos/id/168/889/536.jpg?hmac=3oSZ237Va2MomQuC9nnE7Z2drQYJOQiOES2xb3XBZ_Y"
     },
     {
       "image": "https://i.picsum.photos/id/110/889/536.jpg?hmac=SW0tF36OSr4Pt8MBxWuTacbCUISxKad_kKNhIcWkEPU",
-      "thumbImage": "https://i.picsum.photos/id/110/889/536.jpg?hmac=SW0tF36OSr4Pt8MBxWuTacbCUISxKad_kKNhIcWkEPU",
-      "title": "Image Six"
+      "thumbImage": "https://i.picsum.photos/id/110/889/536.jpg?hmac=SW0tF36OSr4Pt8MBxWuTacbCUISxKad_kKNhIcWkEPU"
     },
     {
       "image": "https://i.picsum.photos/id/916/889/536.jpg?hmac=Vpy97Y1IPFVIZxWmh0O3p-hGT6dx1jKwIyY2gkz3kAw",
-      "thumbImage": "https://i.picsum.photos/id/916/889/536.jpg?hmac=Vpy97Y1IPFVIZxWmh0O3p-hGT6dx1jKwIyY2gkz3kAw",
-      "title": "Image Seven"
+      "thumbImage": "https://i.picsum.photos/id/916/889/536.jpg?hmac=Vpy97Y1IPFVIZxWmh0O3p-hGT6dx1jKwIyY2gkz3kAw"
     },
     {
       "image": "https://i.picsum.photos/id/851/889/536.jpg?hmac=5iCNj4Z0LAI3nlQilYE8qafE14mkao8X-h59spAF9RU",
-      "thumbImage": "https://i.picsum.photos/id/851/889/536.jpg?hmac=5iCNj4Z0LAI3nlQilYE8qafE14mkao8X-h59spAF9RU",
-      "title": "Image Eight"
+      "thumbImage": "https://i.picsum.photos/id/851/889/536.jpg?hmac=5iCNj4Z0LAI3nlQilYE8qafE14mkao8X-h59spAF9RU"
     },
     {
       "image": "https://i.picsum.photos/id/305/889/536.jpg?hmac=y3oUGU8B9yzK4L-LeBafoH8AzZ0O29eDsx7tenR_EyA",
-      "thumbImage": "https://i.picsum.photos/id/305/889/536.jpg?hmac=y3oUGU8B9yzK4L-LeBafoH8AzZ0O29eDsx7tenR_EyA",
-      "title": "Image Nine"
+      "thumbImage": "https://i.picsum.photos/id/305/889/536.jpg?hmac=y3oUGU8B9yzK4L-LeBafoH8AzZ0O29eDsx7tenR_EyA"
     },
     {
       "image": "https://i.picsum.photos/id/584/889/536.jpg?hmac=dghSCV_0viTgB_L9T5wVowf6ghuKVENHgP_NrA7NANQ",
-      "thumbImage": "https://i.picsum.photos/id/584/889/536.jpg?hmac=dghSCV_0viTgB_L9T5wVowf6ghuKVENHgP_NrA7NANQ",
-      "title": "Image Ten"
+      "thumbImage": "https://i.picsum.photos/id/584/889/536.jpg?hmac=dghSCV_0viTgB_L9T5wVowf6ghuKVENHgP_NrA7NANQ"
     },
     {
       "image": "https://i.picsum.photos/id/919/889/536.jpg?hmac=QF3tfAuf7uq_-ER5lEKGmdnbBDEWycZVPbPms8Ap9_M",
-      "thumbImage": "https://i.picsum.photos/id/919/889/536.jpg?hmac=QF3tfAuf7uq_-ER5lEKGmdnbBDEWycZVPbPms8Ap9_M",
-      "title": "Image Eleven"
+      "thumbImage": "https://i.picsum.photos/id/919/889/536.jpg?hmac=QF3tfAuf7uq_-ER5lEKGmdnbBDEWycZVPbPms8Ap9_M"
     },
     {
       "image": "https://i.picsum.photos/id/859/889/536.jpg?hmac=Y916ejeqOlQW7zdKF0y7GBsbGM_Gv2-CdjG5XpqUdrE",
-      "thumbImage": "https://i.picsum.photos/id/859/889/536.jpg?hmac=Y916ejeqOlQW7zdKF0y7GBsbGM_Gv2-CdjG5XpqUdrE",
-      "title": "Image Twelve"
+      "thumbImage": "https://i.picsum.photos/id/859/889/536.jpg?hmac=Y916ejeqOlQW7zdKF0y7GBsbGM_Gv2-CdjG5XpqUdrE"
     },
     {
       "image": "https://i.picsum.photos/id/719/889/536.jpg?hmac=kxPet_1Ao9vqO526pnb7VmdshgcR8I3rUbnGAtszznM",
-      "thumbImage": "https://i.picsum.photos/id/719/889/536.jpg?hmac=kxPet_1Ao9vqO526pnb7VmdshgcR8I3rUbnGAtszznM",
-      "title": "Image Thirteen"
+      "thumbImage": "https://i.picsum.photos/id/719/889/536.jpg?hmac=kxPet_1Ao9vqO526pnb7VmdshgcR8I3rUbnGAtszznM"
     },
     {
       "image": "https://i.picsum.photos/id/242/889/536.jpg?hmac=SfZPauEXOhFk2cWueDjqkE2ctU7IUSh0kdJKGELCudA",
-      "thumbImage": "https://i.picsum.photos/id/242/889/536.jpg?hmac=SfZPauEXOhFk2cWueDjqkE2ctU7IUSh0kdJKGELCudA",
-      "title": "Image Fourteen"
+      "thumbImage": "https://i.picsum.photos/id/242/889/536.jpg?hmac=SfZPauEXOhFk2cWueDjqkE2ctU7IUSh0kdJKGELCudA"
     }
   ]
   
