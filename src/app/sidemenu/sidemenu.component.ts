a03051613ec8b4d06d2707bbb3efd4a4a66435e5
RRathika <shahishaammu03@gmail.com>
2021-11-22 13:09:06 +0530

    updated sweet alert

--- src/app/sidemenu/sidemenu.component.ts
+++ src/app/sidemenu/sidemenu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-sidemenu',
@@ -26,17 +27,31 @@ export class SidemenuComponent {
       this.backgroundtop = "#000";
     }
   }
-  consult(){
-    this.router.navigate(['/consulting'])
-  .then(() => {
-    window.location.reload();
-  });
+  options() {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-warning'
+      },
+      buttonsStyling: true,
+    });
+    swalWithBootstrapButtons.fire(
+    {
+      showCloseButton: true,
+      title: 'Are you sure?',
+      text: 'We will move to another website',
+      showCancelButton: true,
+      confirmButtonText: 'OK',
+      cancelButtonText: 'Cancel',
+      reverseButtons: false
+    }
+    ).then((result) => {
+      if (result.value) {
+        window.open("https://careerin.co.in/", "_blank");
+      }
+      else{
+      console.log('cancel');
+      }
+    });
   }
-  itservice(){
-    this.router.navigate(['/itservice'])
-  .then(() => {
-    window.location.reload();
-  });
-  }
-
 }
